1c7da6ff82896b55d3d6ec7d663e4abd814a294e
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-28 11:32:26 +0000

    🫦

---
+++ WO/sve.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+# Running Svelte related commangs here
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+
+h1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+# --- Executing Svelte Commands ---
